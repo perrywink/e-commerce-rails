e2e155a6a0dcec2b7de3be5af8e540e699a091d5
YiJieChuah <s3847905@student.rmit.edu.au>
2021-05-11 20:06:13 +0000

    Make displayed items clickable to add to list.

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,7 +1,7 @@
 module ItemsHelper
   def in_saved_list?(item)
     if session[:saved_list]
-      session[:saved_list].any?{|i| i["id"] == @rand_item.id }
+      session[:saved_list].any?{|i| i["id"] == item.id }
     end
   end
 end
