71c0632a658b083cf6b9c46e79d70ecc66a7bee6
YiJieChuah <s3847905@student.rmit.edu.au>
2021-05-17 15:55:27 +0000

    Styled devise views. Setup regex validation to match spec reqs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  
   def index
     @items = Item.order('popularity DESC').first(4)
     @collections = Collection.first(4)
@@ -9,7 +12,14 @@ class HomeController < ApplicationController
   
   def newsletter
     email = params[:email]
-    NewsletterMailer.signup_confirmation(email).deliver
-    redirect_to root_path, flash: { success: "Check your inbox!" }
+    if email
+      if email.match(VALID_EMAIL_REGEX)
+        NewsletterMailer.signup_confirmation(email).deliver
+        redirect_to root_path, flash: { success: "Check your inbox!" }
+      else
+        redirect_to root_path, flash: { warning: "Invalid email entered!" }
+      end
+    end
+    
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+         
+  PASSWORD_FORMAT = /\A
+    (?=^\W{8,20})
+  /x
+  
+  validates :password, 
+    presence: true, 
+    length: { in: Devise.password_length }, 
+    format: { with: PASSWORD_FORMAT }, 
+    confirmation: true, 
+    on: :create 
+  
 end
